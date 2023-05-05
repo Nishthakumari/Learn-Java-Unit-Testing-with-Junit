dbb10198706da3e068a6a7f06793a3802467ce29
Nishthakumari <knishtha0@gmail.com>
2023-05-05 23:02:52 +0530

    Step 02 : First Successful JUnit. Green Bar and assertEquals

--- src/test/java/com/in28minutes/junit/helper/JunitCourse/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/JunitCourse/StringHelperTest.java
@@ -2,11 +2,17 @@ package com.in28minutes.junit.helper.JunitCourse;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringHelperTest {
 
     @Test
     public void test()
     {
+        StringHelper helper = new StringHelper();
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+
 
     }
 
