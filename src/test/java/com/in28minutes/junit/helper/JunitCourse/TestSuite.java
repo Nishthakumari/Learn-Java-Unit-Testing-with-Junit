7cfdbb08d9a7d148d487ec516fc0d3111f441195
Nishthakumari <knishtha0@gmail.com>
2023-05-06 17:16:52 +0530

    Step 10 : Organize JUnits into Suites

---
+++ src/test/java/com/in28minutes/junit/helper/JunitCourse/TestSuite.java
@@ -0,0 +1,14 @@
+package com.in28minutes.junit.helper.JunitCourse;
+
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+
+@SelectClasses({ArraysCompareTest.class, QuickBeforeAfterTest.class})
+
+
+@Suite
+public class TestSuite {
+
+}
